8839bcf298b3f4fa931c600d864eec16c63a2e80
Mahir Çınar Girgin <mcgirgin06@gmail.com>
2025-09-10 23:24:51 +0300

    Add page number indicator and optimization #2

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ with open(file_path, "r", encoding="utf-8") as f:
     d = f.read()
     data = json.loads(d)
 
-class ansicolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
+class color:
+    PURPLE = '\033[95m'
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    END = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
@@ -30,13 +30,13 @@ def try_login():
     try:
         imap.login(data["email"], password = data["imap_pass"])
     except:
-        print(f"{ansicolors.FAIL}Login Credentials are incorrect. again. Make sure you are using a gmail account and a valid app password (From https://myaccount.google.com/apppasswords).{ansicolors.ENDC}")
+        print(f"{color.RED}Login Credentials are incorrect. again. Make sure you are using a gmail account and a valid app password (From https://myaccount.google.com/apppasswords).{color.END}")
         return get_data()
 
 def get_data():
     mail = input("please Enter your Gmail adress: ")
     if mail[-10:] != "@gmail.com":
-        print(f"{ansicolors.FAIL}The mail you just entered is not valid. Currently we support only Gmail acoounts.{ansicolors.ENDC}")
+        print(f"{color.RED}The mail you just entered is not valid. Currently we support only Gmail acoounts.{color.END}")
         return get_data()
     data["email"] = mail
     pw = input("Please enter your IMAP password: ")
@@ -47,8 +47,8 @@ def get_data():
         json.dump(data, f, ensure_ascii=False, indent=4)
     try_login()
 
-if data["email"] == "":
-    print(f"{ansicolors.FAIL}You have not entered your email credentials.{ansicolors.ENDC}")
+if data["email"] == "" or data["imap_pass"] == "":
+    print(f"{color.RED}You have not entered your email credentials.{color.END}")
     get_data()
 else:
     try_login()
@@ -84,9 +84,9 @@ def print_results():
     input = await_input()
     print_line()
     if input == "x":
-        page = page + 1
+        page += 1
     elif input == "z" and page != 1:
-        page = page - 1
+        page -= 1
     elif input == "z" and page == 1:
         pass
     elif input == "r":
@@ -106,13 +106,13 @@ def print_results():
         try:
             input = input.split()
         except:
-            print(f"{ansicolors.FAIL}Unexpected command. Example usage 'mpp 5'.{ansicolors.ENDC}")
+            print(f"{color.RED}Unexpected command. Example usage 'mpp 5'.{color.END}")
             print_line()
             return print_results()
         try:
             data["mails_per_page"] = int(input[1])
         except:
-            print(f"{ansicolors.FAIL}Unexpected command. Example usage 'mpp 5'.{ansicolors.ENDC}")
+            print(f"{color.RED}Unexpected command. Example usage 'mpp 5'.{color.END}")
             print_line()
             return print_results()
         with open(file_path, "w", encoding="utf-8") as f:
@@ -121,13 +121,15 @@ def print_results():
         try:
             page = int(input)
         except:
-            print(f"{ansicolors.FAIL}'{input}' command not found.{ansicolors.ENDC}")
+            print(f"{color.RED}'{input}' command not found.{color.END}")
             print_line()
             print_results()
     
     start = page * data["mails_per_page"] - (data["mails_per_page"] - 1)
     end = page * data["mails_per_page"] + 1
     try:
+        print(f"{color.GREEN}PAGE NUMBER: {page}{color.END}")
+        print_line()
         for i in range(start, end):
             num = messages[0].split()[::-1][i-1]
 
@@ -143,13 +145,14 @@ def print_results():
             if isinstance(subject, bytes):
                 subject = subject.decode("utf-8")
 
-            print(f"{ansicolors.HEADER}Subject:{ansicolors.ENDC}", ansicolors.WARNING + subject+ ansicolors.ENDC)
-            print(f"{ansicolors.UNDERLINE}From:{ansicolors.ENDC}", message["From"])
-            print(f"{ansicolors.UNDERLINE}Date:{ansicolors.ENDC}", message["Date"])
-            print(f"{ansicolors.UNDERLINE}Gmail Link:{ansicolors.ENDC}", ansicolors.OKCYAN + gmail_link + ansicolors.ENDC)
+            print(f"{color.PURPLE}Subject:{color.END}", color.YELLOW + subject+ color.END)
+            print(f"{color.UNDERLINE}From:{color.END}", message["From"])
+            print(f"{color.UNDERLINE}Date:{color.END}", message["Date"])
+            print(f"{color.UNDERLINE}Gmail Link:{color.END}", color.CYAN + gmail_link + color.END)
             print_line()
     except:
-        print(f"{ansicolors.FAIL}The number of page you just entered doesn't exist. Please provide another one!{ansicolors.ENDC}")
+        print(f"{color.RED}The number of page you just entered doesn't exist. Please provide another one!{color.END}")
+        print_line()
     print_results()
     print_line()
 
